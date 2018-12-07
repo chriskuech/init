c7d462e7f80a4f4f751c600109578b9509a11a28
Chris Kuech <chkuech@microsoft.com>
2018-12-06 16:25:25 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 
 # install NVM (node version manager)
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+if [ ! command -v nvm ]; then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+fi
+
+if [ ! -d ~/repos ]; then
+  mkdir ~/repos
+fi
