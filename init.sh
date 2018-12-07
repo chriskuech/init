fb6f62b1b42c403b1959ec07ac5b300f46ae18d4
Chris Kuech <chkuech@microsoft.com>
2018-12-07 00:39:44 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
 
 # install NVM (node version manager)
-if [ -n $(command -v nvm) ]; then
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-fi
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 # create ~/repos
-if [ ! -d ~/repos ]; then
-  mkdir ~/repos
-fi
+mkdir ~/repos
+
+# install linuxbrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+# install git credential manager
+brew install git-credential-manager
+git-credential-manager install
+
