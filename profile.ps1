16499051e972946bc9835b9eeaf86f02c807f67b
Chris Kuech <chkuech@microsoft.com>
2019-01-03 14:17:16 -0800

    added git cmdlets to profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,8 +1,48 @@
 
+function Get-Branch {
+  git branch --format "%(refname:short)"
+}
+
+function Get-Origin {
+  (git config remote.origin.url) -replace "\.git$"
+}
+
+function New-PullRequest {
+  $branch = Get-Branch
+  $branchEscaped = [uri]::EscapeUriString($branch)
+  $origin = Get-Origin
+  if ($origin -like "*.visualstudio.com*" -or $origin -like "*dev.azure.com*") {
+    Start-Process "$origin/pullrequestcreate?sourceRef=$branchEscaped&targetRef=master"
+  }
+  else {
+    $origin -match "https?://([^/]+)" | Out-Null
+    $hostname = $Matches[1]
+    throw "Git host '$hostname' is not supported by this cmdlet"
+  }
+}
 function Open-Branch {
-  $origin = git config remote.origin.url
-  $branch = git branch --format "%(refname:short)"
-  start "$origin/$branch"
+  $branch = Get-Branch
+  $origin = Get-Origin
+  if ($origin -like "*github.com*") {
+    Start-Process "$origin/tree/$branch"
+  }
+  elseif ($origin -like "*.visualstudio.com*" -or $origin -like "*dev.azure.com*") {
+    Start-Process "$origin/$branch"
+  }
+  else {
+    $origin -match "https?://([^/]+)" | Out-Null
+    $hostname = $Matches[1]
+    throw "Git host '$hostname' is not supported by this cmdlet"
+  }
+}
+
+function Publish-Branch {
+  git push --set-upstream origin (Get-Branch)
+}
+
+function Repair-Git {
+  git gc --prune=now --aggressive
+  git remote prune origin
 }
 
 Set-Location "C:\Repos"
