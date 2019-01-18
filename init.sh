025d0d225101ecaa0f2e50ec8b2fa72aa461c66a
Chris Kuech <chkuech@microsoft.com>
2019-01-18 08:40:08 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,10 @@
 # install NVM (node version manager)
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
+# load NVM (required as of Jan 2019)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
 # create ~/repos
 mkdir ~/repos
 
@@ -17,10 +21,16 @@ echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 brew install git-credential-manager
 git-credential-manager install
 
-# install f.lux (currently fails)
+# install f.lux (fails as of Dec 2018)
 # sudo add-apt-repository ppa:nathan-renniewaldock/flux
 # sudo apt-get update
 # sudo apt-get install fluxgui
 
 # (almost) fix trackpad driver on kubuntu
 sudo apt install xserver-xorg-input-synaptics
+
+# set default directory (macOS Terminal feature shim Jan 2018)
+$DEAFULTDIR="~/Projects"
+if [ "$OLDPWD" ] && [ $PWD = $HOME ]; then
+    cd $DEAFULTDIR
+fi
