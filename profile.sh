703387165a702739a67f46170aa5c8e4788a19fc
Chris Kuech <chkuech@microsoft.com>
2019-01-19 11:53:23 -0800

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -1,3 +1,16 @@
 
+# enable piping strings to clipboard
 alias clip='xclip -sel clip'
-cd ~/repos
+
+# load NVM (required as of Jan 2019)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# enable clis installed with pip (macOS)
+export PATH="$HOME/Library/Python/2.7/bin:$PATH"
+
+# set default directory (macOS Terminal feature shim Jan 2018)
+DEFAULTDIR="$HOME/Projects"
+if [ -z "$OLDPWD" ] && [ $PWD = $HOME ]; then
+    cd $DEFAULTDIR
+fi
