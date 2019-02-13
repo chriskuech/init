1a922f2f772f259e851a8525043cad77d20fc7ec
Chris Kuech <chkuech@microsoft.com>
2019-02-12 17:26:59 -0800

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -14,3 +14,17 @@ DEFAULTDIR="$HOME/Projects"
 if [ -z "$OLDPWD" ] && [ $PWD = $HOME ]; then
     cd $DEFAULTDIR
 fi
+
+# use firefox command (macOS)
+alias firefox="/Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox"
+
+# change bash prompt text
+#  https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
+export CLICOLOR=1
+DEFAULTCOLOR="\\033[0m"
+DARKGRAY="\\033[38;5;238m"
+BOLDLIGHTCYAN="\\033[38;5;51;1m"
+export PS1="\[$DARKGRAY\]\A\[$BOLDLIGHTCYAN\]\W\[$DARKGRAY\]▷ \[$DEFAULTCOLOR\]"
+
+# use firefox command
+alias firefox="/Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox"
